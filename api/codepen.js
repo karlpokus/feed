1833c07d80f158c19db0361ab9dcf69afb41623e
Karl Pokus <karl.pokus@gmail.com>
2016-07-08 13:23:55 +0000

    add human readable string

--- api/codepen.js
+++ api/codepen.js
@@ -1,8 +1,38 @@
-var http = require('http'),
-    cheerio = require('cheerio'),
-    url = 'http://codepen.io/KarlPokus/public/feed/';
+var fs = require('fs'),
+    path = require("path"),
+    //http = require('http'),
+    //url = 'http://codepen.io/KarlPokus/public/feed/',
+    cheerio = require('cheerio');
 
 module.exports = function(cb) {
+  
+  // temp read from file
+  fs.readFile(path.join(__dirname, '..', 'temp/codepen.html'), 'utf8', function(err, data) {
+    if (err) return cb(err);
+    
+    var pens = [],
+        $ = cheerio.load(data);
+    
+    $('item').each(function(){
+        pens.push({
+          title: $(this).find('title').text(),
+          url: $(this).find('link').text(),
+          ts: $(this).find('dc\\:date').text()
+        });
+      });
+
+    // [XXXX-XX-XX] edited <title> OR <url>
+    pens.map(function(o){
+      o.dateString = "[" + o.ts.substr(0, 10) + "]"
+      o.str = o.dateString + ' edited ' + o.title;
+      return o;
+    });
+    
+    cb(null, pens);
+    
+  });
+  
+  /*
   http.get(url, function(res){
 
     var data = "";
@@ -28,4 +58,6 @@ module.exports = function(cb) {
   }).on('error', function(err){
     cb(err);
   });
+  */
+  
 }
