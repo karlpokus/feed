4ba24055271803300694f2c893330a0924347651
Karl Pokus <karl.pokus@gmail.com>
2016-07-08 13:24:41 +0000

    temp disable server for testing

--- server.js
+++ server.js
@@ -2,15 +2,32 @@ var http = require("http"),
     fs = require('fs'),
     cheerio = require('cheerio'),
     pens = require('./api/codepen'),
-    server = http.createServer();
+    gits = require('./api/github');
+    //server = http.createServer();
 
-server.on("request", function(req, res) {
-
-  // codepen
-  pens(function(err, data){
-    if (err) throw err;
-    console.log(data[0].title);
+// TESTs
+pens(function(err, data){
+  if (err) throw err;
+  
+  data.forEach(function(o){
+    console.log(o.str);
   });
+});
+
+gits(function(err, data){
+  if (err) throw err;
+  
+  data.forEach(function(o){
+    if (o.str) {
+      console.log(o.str);
+    }    
+  });
+});
+
+// order
+
+/*
+server.on("request", function(req, res) {
 
   // todo: make this a stream
   fs.readFile('./index.html', 'utf8', function(err, data) {
@@ -20,13 +37,14 @@ server.on("request", function(req, res) {
     var li = "<li>Much li is here</li>"
     $('ul').append(li);
 
+    res.setHeader("Content-Type", "text/html");
     res.end($.html());
 
   });
-
 
 });
 
 server.listen(3000, function() {
   console.log('Server running..');
 });
+*/
