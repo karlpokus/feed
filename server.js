e638ae177af874a4479af7b20bc54c87c5e219e3
Karl Pokus <karl.pokus@gmail.com>
2016-07-12 12:41:20 +0200

    add arr stack to requestHandler

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var pype = require('./lib/pype'),
     html = require('./lib/html'),
     pens = require('./api/codepen'),
     gits = require('./api/github'),
+    sos = require('./api/so'),
     http = require("http"),
     server = http.createServer(),
     errorhandler = function(err, req, res) {
@@ -13,10 +14,11 @@ var pype = require('./lib/pype'),
     finalHandler = function(req, res) {
       res.setHeader("Content-Type", "text/html");
       res.end(req.html);
-    };
+    },
+    stack = [pens, gits, sos, concat, index, sort, html, finalHandler];
 
 server.on('request', function(req, res){
-  var requestHandler = pype(null, [pens, gits, concat, index, sort, html, finalHandler], errorhandler);
+  var requestHandler = pype(null, stack, errorhandler);
   requestHandler(req, res);
 });
 
